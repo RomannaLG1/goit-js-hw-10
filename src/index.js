9f661b15f0d113e7edc3d84909b3b572ae7bf84a
RomannaLG1 <melnykromasja@gmail.com>
2022-10-10 21:48:03 +0300

    add template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
+
+fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages')
+  .then(response => {
+    return response.json();
+  })
+  .then(countreName => {
+    console.log(countreName);
+  })
+  .catch(error => {
+    console.log(error);
+  });
