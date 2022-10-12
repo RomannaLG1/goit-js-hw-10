5a1a108feee820ad6606f904e4fe7d0ed6533649
RomannaLG1 <104714229+RomannaLG1@users.noreply.github.com>
2022-10-12 08:29:54 +0000

    Deploying to gh-pages from @ RomannaLG1/goit-js-hw-10@e098f1786d97970667acea5f31e9e8a7730604f6 🚀

--- index.c02a29f8.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/all?fields=name,capital,population,flags,languages").then((l=>l.json())).then((l=>{console.log(l)})).catch((l=>{console.log(l)}));
-//# sourceMappingURL=index.c02a29f8.js.map
--- index.c974b18f.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/all?fields=name,capital,population,flags,languages").then((function(n){return n.json()})).then((function(n){console.log(n)})).catch((function(n){console.log(n)}));
-//# sourceMappingURL=index.c974b18f.js.map
