e098f1786d97970667acea5f31e9e8a7730604f6
RomannaLG1 <melnykromasja@gmail.com>
2022-10-12 11:29:14 +0300

    add fetch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import templateCard from './templates.hbs';
 
-// const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300;
 
-fetch('https://restcountries.com/v2/all?fields=name,capital,population,flags,languages')
-  .then(response => {
-    return response.json();
-  })
-  .then(countreName => {
-    console.log(countreName);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+const listEl = document.querySelector('.country-list');
+const inputEl = document.querySelector('#search-box');
+inputEl.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+
+function onFormInput(evt) {
+  evt.preventDefault();
+
+  fetchCountry().then(renderCountryCard);
+}
+
+function renderCountryCard(country) {
+  const markup = templateCard(country);
+  listEl.innerHTML = markup;
+}
+
+function fetchCountry(countryName) {
+  countryName = inputEl.value;
+  return fetch(
+    `https://restcountries.com/v2/name/${countryName}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      return response.json();
+    })
+   
+}
